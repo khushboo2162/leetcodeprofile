654329aa38c2a8eacb051a585bfb8e790ca7954a
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2026-04-24 14:24:33 +0530

    day 3 ques 5

---
+++ delete_node.java
@@ -0,0 +1,33 @@
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class  delete_node {
+
+    public static void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(9);
+
+        deleteNode(head.next);
+
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+    }
+}
