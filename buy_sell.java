4d6aea4dca20a0f24519aac510ee39c1a5153439
khsuhboo2162 <khushbooagrawal8077@gmail.com>
2026-04-24 09:03:45 +0530

    day3 ques5

---
+++ buy_sell.java
@@ -0,0 +1,24 @@
+public class buy_sell {
+     
+    public int maxProfit(int[] prices) {
+
+        int profit = 0;
+        int buy = prices[0];
+
+        for (int i = 0; i < prices.length; i++) {
+            if (buy > prices[i]) {
+                buy = prices[i];
+            }
+            profit = Math.max(profit, prices[i] - buy);
+        }
+
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        buy_sell obj = new buy_sell();
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(obj.maxProfit(prices));
+    }
+}
+    
